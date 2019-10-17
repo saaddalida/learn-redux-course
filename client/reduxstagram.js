6d913aed8b1a61c1b622d537a2d63d77ed3b4ad1
saaddalida <dalidassaad@gmail.com>
2019-10-17 19:23:47 -0300

    chore: routes setup

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Route, Router, IndexRoute, browserHistory } from 'react-router';
 
 import css from './styles/style.styl';
 
-render(<p>hi</p>, document.getElementById('root'));
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+);
+
+render(router, document.getElementById('root'));
